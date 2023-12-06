138648e410c809be61e8f78bca9c70906af0d700
E <e@pastel.codes>
2023-12-06 10:08:53 +0000

    Change folder structure and add pre-commit and tools

--- wfc/main.py
+++ wfc/main.py
@@ -1,7 +1,8 @@
 from PIL import Image
-from core import wfc_core
-from postprocess import wfc_post_process_image
-from preprocess import wfc_pre_process_image
+
+from wfc.core import wfc_core
+from wfc.preprocess import wfc_pre_process_image
+from wfc.postprocess import wfc_post_process_image
 
 
 def wfc_image(image: Image, tile_size: int, output_size: (int, int)) -> Image:
@@ -9,10 +10,12 @@ def wfc_image(image: Image, tile_size: int, output_size: (int, int)) -> Image:
     tile_index_grid = wfc_core(adjacency_rules, frequency_hints, output_size)
     return wfc_post_process_image(tile_index_grid, top_left_pixel_of_each_tile)
 
-
-if __name__ == "__main__":
+def main():
     print("running...")
     with Image.open("tile.png") as im:
         im.load()
         wfc_image(im, 3, (300, 300))
     print("done!")
+
+if __name__ == "__main__":
+    main()
